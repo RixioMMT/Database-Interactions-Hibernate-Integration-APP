098a7e39b211c32c575641f149bb6ebc2e044a8c
RixioMMT <rixio1994@gmail.com>
2024-03-17 16:20:16 -0400

    Second commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.*;
+import org.hibernate.query.NativeQuery;
 import java.util.List;
 
-public class UserDaoHibernateImpl implements UserDao {
+public class UserDaoHibernateImpl extends Util implements UserDao {
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +14,91 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        String query = "CREATE TABLE IF NOT EXISTS RIXIOTABLE (" +
+                "id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(20), " +
+                "lastName VARCHAR(20), " +
+                "age TINYINT)";
+        try (Session session = getSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(query).executeUpdate();
+            transaction.commit();
+            System.out.println("Table created successfully");
+        }
+        catch (HibernateException e) {
+            System.out.println("Error at creating table");
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String query = "DROP TABLE IF EXISTS RIXIOTABLE";
+        try (Session session = getSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(query).executeUpdate();
+            transaction.commit();
+            System.out.println("Table dropped successfully");
+        }
+        catch (HibernateException e) {
+            System.out.println("Error at dropping table");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = getSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+            System.out.println("User saved successfully");
+        }
+        catch (HibernateException e) {
+            System.out.println("Error at saving user");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = getSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+            System.out.println("User removed successfully");
+        }
+        catch (HibernateException e) {
+            System.out.println("Error at removing user");
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String query = "SELECT id, name, lastName, age FROM RIXIOTABLE";
+        List<User> users = null;
+        try (Session session = getSession()) {
+            Transaction transaction = session.beginTransaction();
+            NativeQuery<User> nativeQuery = session.createNativeQuery(query, User.class);
+            users = nativeQuery.list();
+            transaction.commit();
+            System.out.println("Users retrieved successfully");
+        } catch (HibernateException e) {
+            System.out.println("Error at retrieving users");
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        String query = "DELETE FROM RIXIOTABLE";
+        try (Session session = getSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createNativeQuery(query).executeUpdate();
+            transaction.commit();
+            System.out.println("Table cleaned successfully");
+        }
+        catch (HibernateException e) {
+            System.out.println("Error at cleaning table");
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,22 +1,60 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
     private static final String driver = "com.mysql.cj.jdbc.Driver";
     private static final String url = "jdbc:mysql://localhost:3306/rixio";
+
     public Connection getConnection() {
         Connection connection = null;
-        try{
+        try {
             Class.forName(driver);
             connection = DriverManager.getConnection(url, "root", "mysqlapesta123");
             System.out.println("Connection success");
-        }
-        catch(ClassNotFoundException | SQLException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Connection error");
         }
         return connection;
     }
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        SessionFactory sessionFactory = null;
+        try {
+            Map<String, String> settings = new HashMap<>();
+            settings.put("hibernate.connection.driver_class", driver);
+            settings.put("hibernate.connection.url", url);
+            settings.put("hibernate.connection.username", "root");
+            settings.put("hibernate.connection.password", "mysqlapesta123");
+            settings.put("dialect", "org.hibernate.dialect.MySQL8Dialect");
+            settings.put("hibernate.current_session_context_class", "thread");
+            ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(settings).build();
+            Metadata metadata = new MetadataSources(registry).addAnnotatedClass(User.class).getMetadataBuilder().build();
+            sessionFactory = metadata.getSessionFactoryBuilder().build();
+            System.out.println("Connection success");
+        }
+        catch (HibernateException e) {
+            System.out.println("Connection error");
+        }
+        return sessionFactory;
+    }
+    public Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
 }
+
